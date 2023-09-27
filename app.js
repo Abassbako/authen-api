0fca983d8e553823725219dcebe05fd08215bbed
Abassbako <abassbako22@gmail.com>
2023-09-27 02:01:20 +0100

    No commit message

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const userRoutes = require('../server/router/userRoutes');
+const userRoutes = require('./server/router/userRoutes');
 
 const cors = require('cors');
 require('dotenv').config();
